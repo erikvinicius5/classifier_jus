0c03f8d51c1c5f4147c4970ee5b159be674b383b
Jusbrasil <jusbrasil@jusbrasil-P53G.(none)>
2014-08-13 18:28:20 -0300

    Falta acertar join

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from splinter import Browser
 from time import sleep
-from threading import Semaphore
+from threading import Semaphore, Thread
 from bs4 import BeautifulSoup
+import os
 
 import urllib
 
 category_links = {
 	"Direito militar": "http://www.jusbrasil.com.br/topicos/292125/direito-militar/artigos",
-	"Direito do trabalho": "http://www.jusbrasil.com.br/topicos/355654/direito-do-trabalho/artigos",
-	"Direito agrario": "http://www.jusbrasil.com.br/topicos/26413195/direito-agrario/artigos",
-	"Direito ambiental": "http://www.jusbrasil.com.br/topicos/26413196/direito-ambiental/artigos",
-	"Direito civil": "http://www.jusbrasil.com.br/topicos/26413200/direito-civil/artigos",
-	"Direito consitucional": "http://www.jusbrasil.com.br/topicos/26413203/direito-constitucional/artigos",
-	"Direito de energia": "http://www.jusbrasil.com.br/topicos/26413204/direito-de-energia/artigos",
-	"Direito de familia": "http://www.jusbrasil.com.br/topicos/26413205/direito-de-familia/artigos",
-	"Direito do consumidor": "http://www.jusbrasil.com.br/topicos/26413213/direito-do-consumidor/artigos",
-	"Direito de turismo": "http://www.jusbrasil.com.br/topicos/26413215/direito-do-turismo/artigos",
-	"Direito de internet": "http://www.jusbrasil.com.br/topicos/26413216/direito-de-internet/artigos",
-	"Direito eleitoral": "http://www.jusbrasil.com.br/topicos/26413218/direito-eleitoral/artigos",
-	"Direito financeiro": "http://www.jusbrasil.com.br/topicos/26413222/direito-financeiro/artigos",
-	"Direito medico": "http://www.jusbrasil.com.br/topicos/26413226/direito-medico/artigos",
-	"Direito internacional": "http://www.jusbrasil.com.br/topicos/26413224/direito-internacional/artigos",
-	"Direito publico": "http://www.jusbrasil.com.br/topicos/26413234/direito-publico/artigos",
-	"Direitos humanos": "http://www.jusbrasil.com.br/topicos/26413243/direitos-humanos/artigos",
+#	"Direito do trabalho": "http://www.jusbrasil.com.br/topicos/355654/direito-do-trabalho/artigos",
+#	"Direito agrario": "http://www.jusbrasil.com.br/topicos/26413195/direito-agrario/artigos",
+#	"Direito ambiental": "http://www.jusbrasil.com.br/topicos/26413196/direito-ambiental/artigos",
+#	"Direito civil": "http://www.jusbrasil.com.br/topicos/26413200/direito-civil/artigos",
+#	"Direito consitucional": "http://www.jusbrasil.com.br/topicos/26413203/direito-constitucional/artigos",
+#	"Direito de energia": "http://www.jusbrasil.com.br/topicos/26413204/direito-de-energia/artigos",
+#	"Direito de familia": "http://www.jusbrasil.com.br/topicos/26413205/direito-de-familia/artigos",
+#	"Direito do consumidor": "http://www.jusbrasil.com.br/topicos/26413213/direito-do-consumidor/artigos",
+#	"Direito de turismo": "http://www.jusbrasil.com.br/topicos/26413215/direito-do-turismo/artigos",
+#	"Direito de internet": "http://www.jusbrasil.com.br/topicos/26413216/direito-de-internet/artigos",
+#	"Direito eleitoral": "http://www.jusbrasil.com.br/topicos/26413218/direito-eleitoral/artigos",
+#	"Direito financeiro": "http://www.jusbrasil.com.br/topicos/26413222/direito-financeiro/artigos",
+#	"Direito medico": "http://www.jusbrasil.com.br/topicos/26413226/direito-medico/artigos",
+#	"Direito internacional": "http://www.jusbrasil.com.br/topicos/26413224/direito-internacional/artigos",
+#	"Direito publico": "http://www.jusbrasil.com.br/topicos/26413234/direito-publico/artigos",
+#	"Direitos humanos": "http://www.jusbrasil.com.br/topicos/26413243/direitos-humanos/artigos",
 	"Transito": "http://www.jusbrasil.com.br/topicos/735233/transito/artigos"
 }
 
@@ -31,13 +32,13 @@ class Crawler:
 	def __init__(self, base_links):
 		self.base_links = base_links
 		self.links = {}
-		#self.browser = Browser()
-		self.semaphore = Semaphore()
+		self.browser = Browser()
+		self.semaphore = Semaphore(20)
 
 	def expand_page(self, page):
 		self.browser.visit(page)
 
-		for i in range(9):
+		for i in range(1):
 			try:
 				self.browser.find_by_css('.more').click()
 			except:
@@ -53,37 +54,52 @@ class Crawler:
 
 		return to_return
 
-	def parse_articles(self, links):
-		pass
-
 	def write_article(self, category, url):
 		raw_page = urllib.urlopen(url).read()
 		article = self.get_article_from_page(raw_page)
-		article_id = url.split("/")[2]
-		
+		article_id = url.split("/")[4]
+
+		if not os.path.exists(category):
+			os.makedirs(category)
+
+		output = file(category+"/"+article_id, "wt")
+		output.write(article.encode('utf-8', 'ignore'))
+		print("\n---------- Article "+article_id+" was written\n")
 
 	def get_article_from_page(self, page):
 		to_return = ""
 
 		soup = BeautifulSoup(page)
-		to_return += soup.header.find_all("h1")[0].get_text()
-		to_return += soup.header.find_all("h2")[0].get_text()
+
+		try:
+			to_return += soup.find_all("h1")[0].get_text()
+			to_return += soup.find_all("h2")[0].get_text()
+		except:
+			pass
+
 		soup.article.div.extract()
 		to_return += soup.article.get_text()
 
 		return to_return
 
-	def set_links(self, category, links):
-		self.semaphore.acquire()
-		self.links[category] = links
-		self.semaphore.release()
+	def parse_category(self, category, links):
+		for link in links:
+			self.semaphore.acquire()
+			print("Launching thread "+ link)
+			t = Thread(target=self.write_article, args=(category, link, ))
+			t.start()
+			t.join()
+			self.semaphore.release()
 
 	def generate_links(self):
 		output = file("links", "wt")
 		for category in self.base_links:
 
 			self.expand_page(self.base_links[category])
-			self.set_links(category, self.get_links_in_loaded_page())
+			links = self.get_links_in_loaded_page()
+
+			print("Lauching thread "+ category)
+			Thread(target=self.parse_category, args=(category, links,)).start()
 
 		#	output.write(category)
 		#	output.write("\n"+str(len(self.links[category]))+"\n")
@@ -97,8 +113,9 @@ class Crawler:
 		
 
 crawler = Crawler(category_links)
-#crawler.run()
-crawler.get_article_from_page(urllib.urlopen("http://joaovcastello.jusbrasil.com.br/artigos/133227251/breves-consideracoes-informativas-e-juridicas-do-seguro-dpvat?ref=home").read())
+crawler.run()
+#crawler.get_article_from_page(urllib.urlopen("http://joaovcastello.jusbrasil.com.br/artigos/133227251/breves-consideracoes-informativas-e-juridicas-do-seguro-dpvat?ref=home").read())
+
 
 #browser = Browser()
 #browser.visit("http://www.jusbrasil.com.br/topicos/292125/direito-militar/noticias")
